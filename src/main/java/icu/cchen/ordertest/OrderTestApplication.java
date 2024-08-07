fa83caa460c4cd16b25d225540b18528b2a0ea0b
罗汉 <1072344372@qq.com>
2024-08-07 16:33:15 +0800

    测试推送

---
+++ src/main/java/icu/cchen/ordertest/OrderTestApplication.java
@@ -0,0 +1,13 @@
+package icu.cchen.ordertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/cchen/ordertest/OrderTestApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.cchen.ordertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
